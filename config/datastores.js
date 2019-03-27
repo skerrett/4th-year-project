a8619410eacff812f1c26e78b2c1b14d24e6b534
skerrett <darrenskerrett96@gmail.com>
2019-03-27 20:08:59 +0000

    changed to clearDB mysql

--- config/datastores.js
+++ config/datastores.js
@@ -1,7 +1,10 @@
 module.exports.datastores = {
   default: {
-    adapter: require('sails-postgresql')	,
-    url: 'postgres://upmlzoptarypap:4592ad421d42ad919f6edd114cd8f00a6af873f7957b93841a446e5026615693@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/d4v4p45ttcdt',
+    //adapter: require('sails-postgresql')	,
+    //url: 'postgres://upmlzoptarypap:4592ad421d42ad919f6edd114cd8f00a6af873f7957b93841a446e5026615693@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/d4v4p45ttcdt',
       //'mysql://newuser:seren@localhost:3306/easy',
+
+    adapter: require('sails-mysql'),
+    url: 'mysql://bb8ccae427d778:2b13e279@eu-cdbr-west-02.cleardb.net/heroku_2efd7961ccc3992?reconnect=true',
   }
 };
--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,6 @@
 var sails_datastores__default__url = 'mysql://newuser:seren@localhost:3306/easy';
-var postgres = 'postgres://upmlzoptarypap:4592ad421d42ad919f6edd114cd8f00a6af873f7957b93841a446e5026615693@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/d4v4p45ttcdtb7'
+var heroku_datastores = 'mysql://bb8ccae427d778:2b13e279@eu-cdbr-west-02.cleardb.net/heroku_2efd7961ccc3992?reconnect=true';
+var postgres = 'postgres://upmlzoptarypap:4592ad421d42ad919f6edd114cd8f00a6af873f7957b93841a446e5026615693@ec2-46-137-113-157.eu-west-1.compute.amazonaws.com:5432/d4v4p45ttcdtb7';
 /**
  * Production environment settings
  * (sails.config.*)
@@ -50,8 +51,11 @@ module.exports = {
     ***************************************************************************/
     default: {
 
-      adapter: require('sails-postgresql'),
-      url: postgres ,
+      //adapter: require('sails-postgresql'),
+      //url: postgres ,
+
+      adapter: require('sails-mysql'),
+      url: heroku_datastores,
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
