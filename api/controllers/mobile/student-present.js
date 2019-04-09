f5e4121ca51513996c9e867479797dc496f923c4
skerrett <darrenskerrett96@gmail.com>
2019-04-10 00:07:41 +0100

    test deploy

--- api/controllers/mobile/student-present.js
+++ api/controllers/mobile/student-present.js
@@ -3,6 +3,7 @@
 module.exports = async function getQuote(req, res) {
    sails.log(req.param('input'));
    var text = req.param('input');
+   var data = {Fname: "Darren",Lname: "Skerrett", nfcCode: "2345682739"};
 
-   return res.json(200,{ nfcCode: 'Hello Darren' });
+   return res.json(200,{ Student: data });
 };
