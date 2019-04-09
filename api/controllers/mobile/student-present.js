950a702fe4235c8dab54af20374f47d0d7a8ee2c
skerrett <darrenskerrett96@gmail.com>
2019-04-10 00:00:18 +0100

    test

--- api/controllers/mobile/student-present.js
+++ api/controllers/mobile/student-present.js
@@ -4,5 +4,5 @@ module.exports = async function getQuote(req, res) {
    sails.log(req.param('input'));
    var text = req.param('input');
 
-   return res.json({ quote: text });
+   return res.json(200,{ nfcCode: 'Hello Darren' });
 };
