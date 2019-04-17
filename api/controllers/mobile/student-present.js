ceb8d714284080e67d3febe079dd78d9853acf17
skerrett <darrenskerrett96@gmail.com>
2019-04-17 16:56:01 +0100

    Update student-present.js

--- api/controllers/mobile/student-present.js
+++ api/controllers/mobile/student-present.js
@@ -2,16 +2,10 @@
 
 module.exports = async function updateAttendance(req, res) {
 
-  var student = await Student.find({}).where({nfcCode: req.param('input')}).populate('subjects');
-
-  sails.log(student[0].subjects);
-  sails.log(student);
-
-  var lesson = await Lesson.find()
-    .where({
-      'date': {'>=': '2019-04-30T10:00:00', '<': '2019-04-30T11:00:00'},
-    });
-
-  sails.log(lesson);
+  var studentId = await Student.find({where: {nfcCode: req.param('input')},
+    select: ['id']});
+  await Attendance.update({lessons: req.param('lesson'),students: studentId.id }).set({
+    isPresent:true
+  });
    return res.ok();
 };
