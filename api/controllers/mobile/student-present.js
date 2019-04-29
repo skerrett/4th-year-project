394fd4f3e26036b990e089f1960be1a86f7189d8
skerrett <darrenskerrett96@gmail.com>
2019-04-29 15:01:38 +0100

    Mobile error

--- api/controllers/mobile/student-present.js
+++ api/controllers/mobile/student-present.js
@@ -2,13 +2,13 @@ module.exports = async function updateAttendance(req, res) {
 
   var studentId = await Student.find({where: {nfcCode: req.param('input')},
     select: ['id']});
+
   //Not Found
   if (studentId === undefined || studentId.length === 0) {
-
   return res.badRequest();
   }
   //Else update
-  await Attendance.update({lessons: req.param('lesson'),students: studentId.id }).set({
+  await Attendance.update({lessons: req.param('lesson'),students: studentId[0].id }).set({
     isPresent:true
   });
    return res.ok();
